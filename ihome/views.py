1c904895ee7707c939d594425c479a67a5d745c0
wancjst <823726926@qq.com>
2020-09-19 22:31:59 +0800

    ihome

--- ihome/views.py
+++
@@ -1,2 +0,0 @@
-
-from ihome import db
--- ihome/web_html.py
+++ ihome/web_html.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 
 
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response
+from flask_wtf import csrf
 
 
 # 提供静态文件的蓝图
@@ -23,8 +24,13 @@ def get_html(html_file_name):
     if html_file_name != "favicon.ico":
         html_file_name = "html/" + html_file_name
 
+    # 创建一个csrf_token值
+    csrf_token = csrf.generate_csrf()
+
     # flask 提供的返回静态文件的方法
-    return current_app.send_static_file(html_file_name)
+    resp = make_response(current_app.send_static_file(html_file_name))
+
+    return resp
 
 
 
